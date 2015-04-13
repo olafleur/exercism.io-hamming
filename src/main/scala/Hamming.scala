9a8765dc8c3720b0b4c375b7a607c86453650425
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-04-13 19:53:31 -0400

    add cleaner in comments

--- src/main/scala/Hamming.scala
+++ src/main/scala/Hamming.scala
@@ -1,4 +1,5 @@
 object Hamming {
+  //shorter (one line!) : (s1 zip s2).count(e => e._1 != e._2)
   def compute(s1: String, s2: String) = {
     if(s1.length > s2.length) returnHamming(s1, s2)
     else returnHamming(s2, s1)
